7edd5fca4f7f9800e6a456e718729e82552451aa
PrashikDev <prashikinfo11@gmail.com>
2026-05-18 11:31:05 +0530

    jsaddfunctiondestrcturing

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+// function sum(...num){
+//     let total = num[0]+num[1]+num[2]
+//     console.log(total)
+// }
+// sum(3,4,5)
+
+// function sum(a,...num){
+//     let total = a+ num[0]+num[1] ---grouping a=3
+//     console.log(total)
+// }
+// sum(3,4,5)
+
+
+// let students={
+//     id: 256455,
+//     name: "prashik",
+//     Age: 22,
+//     class: 'B'
+// }
+// let{id,name,...num}=students  ----------object destructuring
+// console.log(students)
+
