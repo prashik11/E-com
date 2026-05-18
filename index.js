75a2570792ae733511a805b3cbbbc85af62cdad1
PrashikDev <prashikinfo11@gmail.com>
2026-05-18 12:36:54 +0530

    destructure...hello

--- index.js
+++ index.js
@@ -11,12 +11,20 @@
 // sum(3,4,5)
 
 
-// let students={
-//     id: 256455,
-//     name: "prashik",
-//     Age: 22,
-//     class: 'B'
-// }
+let students={
+    id: 256455,
+    name: "prashik",
+    Age: 22,
+    class: 'B'
+}
 // let{id,name,...num}=students  ----------object destructuring
 // console.log(students)
+
+function obj({id, name,...hello}) // -----------------destructuring by other object to main object
+{
+    console.log(id)
+    console.log(name)
+    console.log(hello)
+}
+obj(students)
 
